4a26c0a7be31a4c254b8dbf2a55745530a5a254b
João Victor Moreira <42523848+Johnny-Connor@users.noreply.github.com>
2023-10-08 18:50:19 -0300

    Update IMyInterface.cs
    Typo.

--- Assets/Scripts/#3 - Interfaces/Example/IMyInterface.cs
+++ Assets/Scripts/#3 - Interfaces/Example/IMyInterface.cs
@@ -8,7 +8,7 @@ public interface IMyInterface
     /*
     * In an Interface, every element has to be public. That means that there is no need to
     * use accessors, for everything is set to be public automatically.
-    * Also, since Interfaces don't have any implementation, it's elements shouldn't have any
+    * Also, since Interfaces don't have any implementation, its elements shouldn't have any
     * code blocks ('{}').
     * Note: With 'Default Interface Methods' (from C# 8.0), the mechanisms above are more
     * malleable. However, since Unity doesn't support it, the old methodology will be used
